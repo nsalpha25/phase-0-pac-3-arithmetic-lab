c3503955bd6309fd402098c1d1e643b60d7cfb8b
Elijah Haven <ehaven26@gmail.com>
2022-08-26 15:50:07 -0400

    commit tree f484ee0b847d5477a878cc087a8a2762025c6d41

---
+++ index.js
@@ -0,0 +1,51 @@
+function add(a, b) {
+return a + b
+}
+
+add(290, 3)
+
+function subtract(a, b) {
+return a - b
+}
+
+subtract(290, 3)
+
+function multiply(a, b) {
+return a * b 
+}
+
+multiply(60, 40)
+
+function divide(a, b) {
+return a / b
+}
+
+divide(40, 2)
+
+function increment(number) {
+    return (number += 1)
+}
+
+increment(5)
+
+function decrement(number) {
+    return (number -= 1)
+}
+
+decrement(5)
+
+function makeInt(n) {
+return parseInt(n, 10)
+
+}
+makeInt("3")
+
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
+preserveDecimal("3.22")
+
+
+   
+
+
